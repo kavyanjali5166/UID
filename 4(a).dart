df2dab4151048e527b0665f64ff74ba90613d114
kavyanjali5166 <152371632+kavyanjali5166@users.noreply.github.com>
2025-11-25 06:04:09 +0530

    Update 4(a).dart

--- 4(a).dart
+++ 4(a).dart
@@ -1,49 +1,40 @@
-import 'package:flutter/material.dart';
-void main() {
-  runApp(NavigationDemoApp());
-}
-class NavigationDemoApp extends StatelessWidget {
-  @override 
-  Widget build(BuildContext context) {
-    return MaterialApp (  
-      title: 'Navigator Named Routes Demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/second': (context) => SecondScreen(),
-      },
-    );
-  }
-}
-class HomeScreen extends StatelessWidget {
-  @override  
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home Screen')),
-      body: Center(  
-        child: ElevatedButton(
-          child: Text('Go to Second Screen'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/second');
-          },
-        ),
-      ),
-    );
-  }
-}
-class SecondScreen extends StatelessWidget {
-  @override 
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Second Screen')),
-      body: Center(  
-        child: ElevatedButton(  
-          child: Text('Go Back to Home'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
+import 'package:flutter/material.dart'; 
+void main() => runApp(const MyApp()); 
+class MyApp extends StatelessWidget { 
+const MyApp({super.key}); 
+@override 
+Widget build(BuildContext context) { 
+return MaterialApp( 
+initialRoute: '/', 
+routes: { 
+'/': (context) => const FirstScreen(), 
+'/second': (context) => const SecondScreen(), }, 
+); 
+} 
+} 
+class FirstScreen extends StatelessWidget { const FirstScreen({super.key}); 
+@override 
+Widget build(BuildContext context) { 
+return Scaffold( 
+appBar: AppBar(title: const Text('First Screen')), body: Center(
+child: ElevatedButton( 
+onPressed: () => Navigator.pushNamed(context, '/second'), child: const Text('Go to Second'), 
+), 
+), 
+); 
+} 
+} 
+class SecondScreen extends StatelessWidget { 
+const SecondScreen({super.key}); 
+@override 
+Widget build(BuildContext context) { 
+return Scaffold( 
+appBar: AppBar(title: const Text('Second Screen')), body: Center( 
+child: ElevatedButton( 
+onPressed: () => Navigator.pop(context), 
+child: const Text('Back'), 
+), 
+), 
+); 
+} 
 }
