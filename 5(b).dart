7d3f2f5ff3172c89d4549ae488f081fd04152abc
kavyanjali5166 <152371632+kavyanjali5166@users.noreply.github.com>
2025-11-25 06:05:32 +0530

    Update 5(b).dart

--- 5(b).dart
+++ 5(b).dart
@@ -1,49 +1,10 @@
-import 'package:flutter/material.dart';
-void main(){
-  runApp(SetStateDemoApp());
-}
-class SetStateDemoApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title:'setState Demo',
-      home:SetStateCounterScreen(),
-    );
-  }
-}
-class SetStateCounterScreen extends StatefulWidget{
-  @override
-  _SetStateCounterScreenState createState()=>
-  _SetStateCounterScreenState();
-
-}
-class _SetStateCounterScreenState extends State<SetStateCounterScreen>
-{
-  int _counter=0;
-  void _incrementCounter(){
-    setState((){
-      _counter++;
-    });
-  }
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar:AppBar(
-        title:Text('SetState Example'),
-      ),
-      body:Center(
-        child:Column(
-          mainAxisAlignment:MainAxisAlignment.center,
-          children:[
-            Text('Counter value:$_counter',style:TextStyle(fontSize:24)),
-            SizedBox(height:16),
-            ElevatedButton(
-              onPressed:_incrementCounter,
-              child:Text('Increment Counter'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+import 'dart:io'; 
+void main() { 
+stdout.write('Enter a number: '); 
+int n = int.parse(stdin.readLineSync()!); 
+if (n % 2 == 0) { 
+print('Even Number'); 
+} else { 
+print('Odd Number'); 
+} 
+} 
